1abf127fb22492c366ae9027b64eacd422fc8915
gtiburcio <guilherme.tiburcio001@gmail.com>
2020-10-05 12:39:38 -0300

    first commit

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// rodando o comando `go build comandos.go` ele irá compilar o código
+// rodando o comando `go run comandos.go` ele irá compilar e executar o código
+func main() {
+	fmt.Printf("Outro progama em %s!\n", "Go")
+}
